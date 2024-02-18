203f37f7aaec86d95bb57fc7d6e670aaf0353396
jdeboi <jdeboi@gmail.com>
2024-02-17 23:19:12 -0600

    bug fix

--- app/(scenes)/sink/page.tsx
+++ app/(scenes)/sink/page.tsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import Sink from '../../../components/scenes/Sink';
-import Ocean2 from '@/components/scenes/Ocean2';
 
 const SinkPage: React.FC = () => {
   return (
     <div style={{ height: '100vh' }}>
       <Sink />
-      {/* <Ocean2 /> */}
     </div>
   );
 };
